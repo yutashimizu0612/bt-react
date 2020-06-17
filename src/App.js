e8ec174599894ddfbb65bd907cdfd7a9dae18bda
yutashimizu0612 <s08388yskh@gmail.com>
2020-06-17 21:53:04 +0900

    タスク追加機能を追加

--- src/App.js
+++ src/App.js
@@ -1,76 +1,87 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  const todos = [
-    {
-      id: 1,
-      title: 'test',
-      status: 'imcomplete',
-    },
-    {
-      id: 2,
-      title: 'test2',
-      status: 'imcomplete',
-    },
-  ];
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          title: 'test',
+          status: 'imcomplete',
+        },
+        {
+          id: 2,
+          title: 'test2',
+          status: 'imcomplete',
+        },
+      ],
+    };
+    this.addTodo = this.addTodo.bind(this);
+  }
 
-  const addTodo = (e) => {
+  addTodo = (e) => {
     e.preventDefault();
     const todo = {
-      id: todos.length + 1,
+      id: this.state.todos.length + 1,
       title: e.target.title.value,
       status: 'imcomplete',
     };
+    this.setState({
+      todos: this.state.todos.concat(todo),
+    });
   };
 
-  return (
-    <div className="wrapper">
-      <h1>ToDoリスト</h1>
-      <ul className="progress-list">
-        <li>
-          <input type="radio" name="progress" id="all" />
-          <label for="all">すべて</label>
-        </li>
-        <li>
-          <input type="radio" name="progress" id="working" />
-          <label for="working">作業中</label>
-        </li>
-        <li>
-          <input type="radio" name="progress" id="complete" />
-          <label for="complete">完了</label>
-        </li>
-      </ul>
-      <table class="todo-status">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>コメント</th>
-            <th>状態</th>
-          </tr>
-        </thead>
-        <tbody>
-          {todos.map((todo, index) => (
-            <tr key={index} className="todo-item">
-              <td>{todo.id}</td>
-              <td className="todo-item__title">{todo.title}</td>
-              <td className="button-wrapper">
-                <button>作業中</button>
-                <button>削除</button>
-              </td>
+  render() {
+    return (
+      <div className="wrapper">
+        <h1>ToDoリスト</h1>
+        <ul className="progress-list">
+          <li>
+            <input type="radio" name="progress" id="all" />
+            <label htmlFor="all">すべて</label>
+          </li>
+          <li>
+            <input type="radio" name="progress" id="working" />
+            <label htmlFor="working">作業中</label>
+          </li>
+          <li>
+            <input type="radio" name="progress" id="complete" />
+            <label htmlFor="complete">完了</label>
+          </li>
+        </ul>
+        <table className="todo-status">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>コメント</th>
+              <th>状態</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-      <section>
-        <h2>新規タスクの追加</h2>
-        <form onSubmit={addTodo}>
-          <input type="text" name="title" />
-          <input type="submit" value="追加" className="add-button" />
-        </form>
-      </section>
-    </div>
-  );
+          </thead>
+          <tbody>
+            {this.state.todos.map((todo, index) => (
+              <tr key={index} className="todo-item">
+                <td>{todo.id}</td>
+                <td className="todo-item__title">{todo.title}</td>
+                <td className="button-wrapper">
+                  <button>作業中</button>
+                  <button>削除</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <section>
+          <h2>新規タスクの追加</h2>
+          <form onSubmit={this.addTodo}>
+            <input type="text" name="title" />
+            <input type="submit" value="追加" className="add-button" />
+          </form>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
